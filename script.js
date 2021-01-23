5803f5de99496fd5d45a0bd1a2890ab0c0d70732
Quinton <qnistico@aol.com>
2021-01-23 16:50:15 -0500

    good

--- script.js
+++ script.js
@@ -7,42 +7,16 @@ $( document ).ready(function() {
   new WOW().init();
 });
 
-//line
-var ctxL = document.getElementById("lineChart").getContext('2d');
-var myLineChart = new Chart(ctxL, {
-type: 'line',
-data: {
-labels: ["January", "February", "March", "April", "May", "June", "July"],
-datasets: [{
-label: "My First dataset",
-data: [65, 59, 80, 81, 56, 55, 40],
-backgroundColor: [
-'rgba(105, 0, 132, .2)',
-],
-borderColor: [
-'#f3ba40',
-],
-borderWidth: 2
-},
-{
-label: "My Second dataset",
-data: [28, 48, 40, 19, 86, 27, 90],
-backgroundColor: [
-'rgba(0, 137, 132, .2)',
-],
-borderColor: [
-'#f3ba40',
-],
-borderWidth: 2
-}
-]
-},
-options: {
-responsive: true
-}
+
+
+
+$(window).scroll(function () {
+  if ($(document).scrollTop() == 0) {
+      $('#header_nav').removeClass('tiny');
+  } else {
+      $('#header_nav').addClass('tiny');
+  }
 });
-
-
 
 
 
